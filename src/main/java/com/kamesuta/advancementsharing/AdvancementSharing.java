6fe51433e1b798807be27d0a21ee8f75822ee783
Kamesuta <kamesuta@gmail.com>
2023-05-09 01:23:04 +0900

    プラグイン作成

---
+++ src/main/java/com/kamesuta/advancementsharing/AdvancementSharing.java
@@ -0,0 +1,17 @@
+package com.kamesuta.advancementsharing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancementSharing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
